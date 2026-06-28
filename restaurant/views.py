7931a3352bffee102dcf2df9f88a80e9320ccba0
Kamlesh Singh Karki <kamlesh451982@gmail.com>
2026-06-28 12:24:10 -0500

    Secure Booking API and fix authentication setup

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,23 +1,17 @@
 from rest_framework import viewsets
-from .models import Menu
-from .serializers import MenuSerializer
-
-
-class MenuItemViewSet(viewsets.ModelViewSet):
-    queryset = Menu.objects.all()
-    serializer_class = MenuSerializer
-
+from rest_framework.permissions import IsAuthenticated
 
 from .models import Menu, Booking
 from .serializers import MenuSerializer, BookingSerializer
-from rest_framework import viewsets
 
 
 class MenuItemViewSet(viewsets.ModelViewSet):
     queryset = Menu.objects.all()
     serializer_class = MenuSerializer
+    permission_classes = []  # public menu
 
 
 class BookingViewSet(viewsets.ModelViewSet):
     queryset = Booking.objects.all()
-    serializer_class = BookingSerializer    
+    serializer_class = BookingSerializer
+    permission_classes = [IsAuthenticated]  # secured bookings
