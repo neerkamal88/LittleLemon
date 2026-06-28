d6bfa354b234f271b8dce7ec9964742213e1e7ee
Kamlesh Singh Karki <kamlesh451982@gmail.com>
2026-06-28 11:05:04 -0500

    Add Django models Menu and Booking with MySQL setup

--- restaurant/models.py
+++ restaurant/models.py
@@ -1,3 +1,19 @@
+# Create your models here.
 from django.db import models
 
-# Create your models here.
+class Menu(models.Model):
+    title = models.CharField(max_length=200)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    inventory = models.IntegerField()
+
+    def __str__(self):
+        return self.title
+
+
+class Booking(models.Model):
+    name = models.CharField(max_length=100)
+    no_of_guests = models.IntegerField()
+    booking_date = models.DateTimeField()
+
+    def __str__(self):
+        return self.name
