b76d8bbac667a143020259125d56d8b996f8565d
Kamlesh Singh Karki <kamlesh451982@gmail.com>
2026-06-28 09:05:40 -0500

    Add Django templates, static files, and restaurant app URLs

--- littlelemon/settings.py
+++ littlelemon/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'littlelemon.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'restaurant' / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
